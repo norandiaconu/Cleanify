024d442b6ed3b936f97d51f14ba08a144ffaf81a
Noran Diaconu <diaconuna@appstate.edu>
2018-05-01 23:10:35 -0400

    Added Chrome support

--- Cleanify.js
+++ Cleanify.js
@@ -1,31 +1,29 @@
 // ==UserScript==
-// @name     Cleanify
-// @version  1.0
-// @grant    none
-// @author	 Noran D
-// @match 	 https://open.spotify.com/collection/artists
+// @name         Cleanify
+// @version      1.1
+// @description  Clean Spotify artists page (Chrome/Firefox).
+// @author       Noran D
+// @match        https://open.spotify.com/collection/artists
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // ==/UserScript==
-alert("START");
-
-var list = document.getElementsByClassName("media-object mo-artist");
-var list2 = document.getElementsByClassName("mo-info-name");
-var names = new Array();
-
-for (i=0; i<list.length; i++)
-{
-  names.push(list2[i].text);
-}
-names.sort();
-
-//alert("Artists found: " + list.length);
-
-for (i=0; i<names.length; i++) {
-  for (j=0; j<list.length; j++) {
-    if (list[j].innerHTML.includes(names[i])) {
-    	list[0].parentNode.parentNode.append(list[j]);
-    }
-  }
-}
-var ads = document.getElementsByClassName("ads-container")[0];
-ads.style.visibility = "hidden";
-//alert("DONE");
+$(function(){
+    setTimeout(function(){
+        var list = document.getElementsByClassName("media-object mo-artist");
+        var list2 = document.getElementsByClassName("mo-info-name");
+        var names = [];
+        for (i=0; i<list.length; i++)
+        {
+            names.push(list2[i].text);
+        }
+        names.sort();
+        for (i=0; i<names.length; i++) {
+            for (j=0; j<list.length; j++) {
+                if (list[j].innerHTML.includes(names[i])) {
+                    list[0].parentNode.parentNode.append(list[j]);
+                }
+            }
+        }
+        var ads = document.getElementsByClassName("ads-container")[0];
+        ads.style.visibility = "hidden";
+    }, 500);
+});
