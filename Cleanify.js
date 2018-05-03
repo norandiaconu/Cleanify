b800d80543de5052bafc57819dbcd77f14336501
Noran Diaconu <diaconuna@appstate.edu>
2018-05-02 22:14:17 -0400

    Improved icon layout/sizing and reduced page timeout

--- Cleanify.js
+++ Cleanify.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Cleanify
-// @version      1.1
+// @version      1.2
 // @description  Clean Spotify artists page (Chrome/Firefox).
 // @author       Noran D
 // @match        https://open.spotify.com/collection/artists
@@ -19,11 +19,18 @@ $(function(){
         for (i=0; i<names.length; i++) {
             for (j=0; j<list.length; j++) {
                 if (list[j].innerHTML.includes(names[i])) {
-                    list[0].parentNode.parentNode.append(list[j]);
+                    list[0].parentNode.parentNode.appendChild(list[j]);
+                    list[j].setAttribute("style", "min-width:100px; max-width:100px;");
                 }
             }
         }
+        for (i=0; i<list.length; i++)
+        {
+            list[0].parentNode.removeChild(list[0].parentNode.firstChild);
+        }
+        var list3 = document.getElementsByClassName("media-object mo-artist");
+        list3[0].parentNode.appendChild(list3[list3.length - 1]);
         var ads = document.getElementsByClassName("ads-container")[0];
         ads.style.visibility = "hidden";
-    }, 500);
+    }, 400);
 });
